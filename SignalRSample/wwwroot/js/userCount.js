f7d8e146a62c1920f34c3e41f2b9e9ac4ae24b03
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-25 17:18:12 -0400

    4.19 SignalR Hub methods

---
+++ SignalRSample/wwwroot/js/userCount.js
@@ -0,0 +1,32 @@
+﻿//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes aka receive notfications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods aka send notification to hub
+
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+function rejected() {
+    //rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
