20bdc5e0529bf982c53136800e21e23a878796f6
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-26 17:50:23 -0400

    3-2 Transport types and Logging in SignalR

--- SignalRSample/wwwroot/js/userCount.js
+++ SignalRSample/wwwroot/js/userCount.js
@@ -1,34 +1,38 @@
 ﻿//create connection
-var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+var connectionUserCount = new signalR.HubConnectionBuilder()
+  //.withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets)
+  .withUrl("/hubs/userCount", signalR.HttpTransportType.ServerSentEvents)
+  //.withUrl("/hubs/userCount", signalR.HttpTransportType.LongPolling)
+  .build();
 
 //connect to methods that hub invokes aka receive notfications from hub
 connectionUserCount.on("updateTotalViews", (value) => {
-    var newCountSpan = document.getElementById("totalViewsCounter");
-    newCountSpan.innerText = value.toString();
+  var newCountSpan = document.getElementById("totalViewsCounter");
+  newCountSpan.innerText = value.toString();
 });
 
 //invoke hub methods aka send notification to hub
 
-
 connectionUserCount.on("updateTotalUsers", (value) => {
-    var newCountSpan = document.getElementById("totalUsersCounter");
-    newCountSpan.innerText = value.toString();
+  var newCountSpan = document.getElementById("totalUsersCounter");
+  newCountSpan.innerText = value.toString();
 });
 
 function newWindowLoadedOnClient() {
-    //connectionUserCount.send("NewWindowLoaded");  // send vs invoke
-    connectionUserCount.send("NewWindowLoaded", "Bhrugen").then((value) => console.log(value));
+  //connectionUserCount.send("NewWindowLoaded");  // send vs invoke
+  connectionUserCount
+    .send("NewWindowLoaded", "Bhrugen")
+    .then((value) => console.log(value));
 }
-
 
 //start connection
 function fulfilled() {
-    //do something on start
-    console.log("Connection to User Hub Successful");
-    newWindowLoadedOnClient();
+  //do something on start
+  console.log("Connection to User Hub Successful");
+  newWindowLoadedOnClient();
 }
 function rejected() {
-    //rejected logs
+  //rejected logs
 }
 
 connectionUserCount.start().then(fulfilled, rejected);
