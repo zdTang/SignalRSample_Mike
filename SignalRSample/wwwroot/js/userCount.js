aaba6038211a5c3198181df36ed7d59ec3071237
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-05-29 11:21:54 -0400

    3-2  Revert .Net 7 to .Net 6
    In order to use old version VS to use JS type intilisense

--- SignalRSample/wwwroot/js/userCount.js
+++ SignalRSample/wwwroot/js/userCount.js
@@ -2,7 +2,7 @@
 var connectionUserCount = new signalR.HubConnectionBuilder()
   .configureLogging(signalR.LogLevel.Trace)
   //.withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets)
-  .withUrl("/hubs/userCount", signalR.HttpTransportType.ServerSentEvents)
+  .withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets)
   //.withUrl("/hubs/userCount", signalR.HttpTransportType.LongPolling)
   .build();
 
